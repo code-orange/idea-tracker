829a261bd7cc4652a9b00ad862b6be58a4c06b42
Tim van Dalen <Tim@timvdalen.nl>
2016-11-05 23:01:51 +0100

    Implement idea component TODO: Don't update contenteditable's when they have focus, as this messes up the cursor position

--- src/app/main/idea/idea.component.ts
+++ src/app/main/idea/idea.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, Input, ViewChild } from '@angular/core';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
 	selector: 'app-idea',
@@ -8,14 +9,33 @@ import { Component, OnChanges, SimpleChanges, Input } from '@angular/core';
 export class IdeaComponent implements OnChanges {
 	abstract;
 
+	@ViewChild('targetelem')
+	targetelem;
+
 	@Input('name')
 	idea_name = '';
 
-	constructor() {
+	idea: FirebaseObjectObservable<any>;
+
+	constructor(private af: AngularFire) {
 	}
 
-	ngOnChanges(changes:SimpleChanges):void {
-		// Reload
-		console.log(changes['idea_name'].currentValue);
+	ngOnChanges(changes:SimpleChanges): void{
+		this.idea = this.af.database.object('/ideas/' + changes['idea_name'].currentValue);
+	}
+
+	update (key: string, value: any) {
+		let change = {};
+		change[key] = value;
+		this.idea.update(change);
+	}
+
+	fileAdded ($event) {
+		let file: File = $event.target.files[0];
+		let reader: FileReader = new FileReader();
+		reader.onloadend = () => {
+			this.update('businessmodel', reader.result);
+		};
+		reader.readAsDataURL(file);
 	}
 }
